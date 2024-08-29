3ea4a20e0324c972ebe04ed0c831b12c8a95a0df
martindissingmadsen <vale-quint08@icloud.com>
2024-08-29 11:40:32 +0000

    Hej

--- src/exercise-5.c
+++ src/exercise-5.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
 
-int main() {
+int main (void) {
 
-    // printf("Hello, World!\n");
-    // int input;
-    // scanf("%d", &input);
-    
+    int a, b;
+
+    printf("Input two numbers:\n"); // User is prompted to enter two numbers
+    scanf("%d, %d", &a, &b);
+
+    if(a > b) // Run the following code if a is larger than b
+    {
+    printf("%d is maximum", a);
+    }
+
+    if(b > a) // Run the following code if b is larger than a
+    {
+    printf("%d is maximum", b);
+    }
+
+    if(a == b) // Run the following code if a is equal to b
+    {
+    printf("%d and %d are equal", a, b);
+    }
+
     return 0;
+
 }
